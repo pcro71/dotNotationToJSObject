28d9dc4d634cf098e19d58f8d9cb026197baf2ec
pcro71 <patrick.j.cross@gmail.com>
2023-12-07 08:38:32 -0800

    cleaned up folder after json output

--- index.js
+++ index.js
@@ -1,44 +1,34 @@
 const fs = require('fs');
 const path = require('path');
+const Papa = require('papaparse');
 
-// Path to the input.txt file
-const inputFile = path.join(__dirname, 'input.txt');
+// Read the CSV file
+const fileContent = fs.readFileSync('input.csv', 'utf8');
 
-// Read the contents of the file
-let fileContents;
-try {
-    fileContents = fs.readFileSync(inputFile, 'utf8');
-} catch (err) {
-    console.error('Error reading the file:', err);
-    return;
+async function parseCSV(fileContent) {
+    return new Promise((resolve, reject) => {
+        Papa.parse(fileContent, {
+            header: false,
+            skipEmptyLines: true,
+            complete: function(results) {
+                resolve(results.data);
+            },
+            error: function(error) {
+                reject(error);
+            }
+        });
+    });
 }
-
-// Split the file content into lines
-const inputs = fileContents.split(/\r?\n/).map(line => {
-    // Trim the line
-    line = line.trim();
-
-    // Escape single quotes and replace double quotes with single quotes
-    //line = line.replace(/'/g, "\\'").replace(/"/g, '"').replace(/\\'/g, "'").replace(/"/g, '');
-    line = line.replace(/'/g, "\\'").replace(/"/g, '"').replace(/\\'/g, "'");
-
-    console.log('lines after processing:', line);
-
-    return line;
-});
-
-
 
 function setValue(obj, path, value) {
     const keys = path.split('.');
     const lastKey = keys.pop();
     const lastKeyIsArray = lastKey.endsWith('[]');
 
-    // iterate through all the object keys on left side of :, create object, and attach to current
     let current = obj;
-    keys.forEach(key => { 
-        if (!current[key]) { // checks if the current object (which initially points to obj) does not have a property with the name of the current key. 
-            current[key] = {}; // creates a new object and assigns it to that property
+    keys.forEach(key => {
+        if (!current[key]) {
+            current[key] = {};
         }
         current = current[key];
     });
@@ -54,53 +44,59 @@ function setValue(obj, path, value) {
     }
 }
 
-function parseAndSet(obj, input) {
+function parseAndSet(obj, innerArray) {
+    if (innerArray.length === 0) {
+        console.log('Empty array');
+        return;
+    }
+
+    let input = innerArray[0];
+    if (input.startsWith("'") && input.endsWith("'")) {
+        input = input.slice(1, -1);
+    }
+
     const colonIndex = input.indexOf(':');
     if (colonIndex === -1) {
         console.log('No colon found in the string');
-        return; // or handle the case as needed
+        return;
     }
 
-    const path = input.substring(0, colonIndex);
-    const rawValue = input.substring(colonIndex + 1);
+    const path = input.substring(0, colonIndex).trim();
+    const rawValue = input.substring(colonIndex + 1).trim();
 
-    // console.log('path:', path);
-    // console.log('rawValue:', rawValue);
+    setValue(obj, path, rawValue);
+}
 
-    const trimmedValue = rawValue.trim();
-    let value;
+(async () => {
+    try {
+        const parsedData = await parseCSV(fileContent);
+        const obj = {};
 
-    // Check if the value is intended to be a JSON object
-    if (trimmedValue.startsWith('{') && trimmedValue.endsWith('}')) {
-        value = trimmedValue;
-        if ((trimmedValue.startsWith("'{") || trimmedValue.startsWith('{"')) && 
-            (trimmedValue.endsWith("}'") || trimmedValue.endsWith('"}'))) {
-            // Remove the first and last characters (quotes and braces)
-            value = trimmedValue.slice(1, -1);
-        }
-    } else {
-        // If not JSON, treat as a string and remove surrounding quotes
-        value = trimmedValue.replace(/^'(.*)'$/, '$1');
+        parsedData.forEach(innerArray => parseAndSet(obj, innerArray));
+
+        // Log the final object here, after processing all data
+        console.log('Final object:', JSON.stringify(obj, null, 2));
+    } catch (error) {
+        console.error("Error: ", error);
     }
+})();
 
-    setValue(obj, path, value);
-}
+(async () => {
+    try {
+        const parsedData = await parseCSV(fileContent);
+        const obj = {};
 
-// Example usage
-const obj = {};
+        parsedData.forEach(innerArray => parseAndSet(obj, innerArray));
 
-inputs.forEach(input => parseAndSet(obj, input));
+        // Save the final object to a file
+        const outputPath = path.join(__dirname, 'output.txt');
+        console.log('Final object:', JSON.stringify(obj, null, 2));
+        console.log('Attempting to write to:', outputPath);
 
-const outputPath = path.join(__dirname, 'output.txt');
+        fs.writeFileSync(outputPath, JSON.stringify(obj, null, 2), 'utf8');
+        console.log('Output saved to', outputPath);
 
-console.log('Final object:', JSON.stringify(obj, null, 2));
-console.log('Attempting to write to:', outputPath);
-
-try {
-    // Explicitly specifying encoding to ensure correct file format
-
-    fs.writeFileSync(outputPath, JSON.stringify(obj, null, 2), 'utf8');
-    console.log('Output saved to', outputPath);
-} catch (err) {
-    console.error('Error writing to file:', err);
-}
+    } catch (error) {
+        console.error("Error: ", error);
+    }
+})();
